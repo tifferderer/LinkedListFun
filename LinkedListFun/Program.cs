4d4bcca8787d61ccd7e2f75dc68f7975235cf263
TIFFANY FERDERER <Tferderer@mail.greenriver.edu>
2021-10-08 16:44:05 -0700

    Add project files.

---
+++ LinkedListFun/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace LinkedListFun
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            links.DoublyLinkedList example = new links.DoublyLinkedList();
+
+            example.add("a");
+            example.add("b");
+            example.add("c");
+            example.add("d");
+            example.add("e");
+
+            Console.WriteLine($"Doubly Linked List: {example.ToString}");
+
+            Console.WriteLine("Lets remove a letter and see if we get a true value back...");
+            Console.WriteLine(example.remove("a"));
+
+            Console.WriteLine("Lets remove a nonexistent letter and see if we get a false value back...");
+            Console.WriteLine(example.remove("a"));
+
+            Console.WriteLine("Look at the Linked List now...");
+            Console.WriteLine(example.ToString);
+        }
+    }
+}
---
+++ LinkedListFun/links/DoublyLinkedList.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListFun.links
+{
+    class DoublyLinkedList
+    {
+        private DoublyLinkedNode head = null;
+        private DoublyLinkedNode tail = null;
+        private int size = 0;
+
+        public void Add(Object data)
+        {
+            if (head == null)
+            {
+                head = new DoublyLinkedNode(data);
+                tail = head;
+
+            }
+            else
+            {
+                tail.next = new DoublyLinkedNode(data, null, tail);
+                tail = tail.next;
+            }
+            size++;
+        }
+
+        public Boolean Remove(Object data)
+        {
+            if (head == null)
+            {
+                return false;
+            }
+
+            if (tail.data.Equals(data))
+            {
+                tail = tail.previous;
+                tail.next = null;
+                size--;
+                return true;
+            }
+
+            if (head.data.Equals(data))
+            {
+                head = head.next;
+                head.previous = null;
+                size--;
+                return true;
+            }
+            DoublyLinkedNode current = tail;
+            while (current.previous != null && !current.previous.data.Equals(data))
+            {
+                current = current.previous;
+            }
+
+
+
+            return false;
+        }
+
+        public new string ToString
+        {
+            get
+            {
+                if (head == null)
+                {
+                    return "The list is empty";
+                }
+                else
+                {
+                    string result = "h > " + head.data;
+                    DoublyLinkedNode current = head.next;
+                    while (current != null)
+                    {
+                        result += " > " + current.data;
+                        current = current.next;
+                    }
+                    result += " > null";
+                    return result;
+                }
+            }
+        }
+
+        public class DoublyLinkedNode
+        {
+            public Object data = null;
+            public DoublyLinkedNode previous = null;
+            public DoublyLinkedNode next = null;
+
+            public DoublyLinkedNode(Object data)
+            {
+                this.data = data;
+            }
+
+            public DoublyLinkedNode(Object data, DoublyLinkedNode next, DoublyLinkedNode previous)
+            {
+                this.data = data;
+                this.next = next;
+                this.previous = previous;
+            }
+
+            public Object getData()
+            {
+                return data;
+            }
+        }
+    }
+    }
